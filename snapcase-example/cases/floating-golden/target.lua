00892f067a3f0e74c9c7aa581357faaa70211fe2
kyoh86 <me@kyoh86.dev>
2026-01-20 05:30:26 +0900

    chore: tweak floating-golden target text

--- snapcase-example/cases/floating-golden/target.lua
+++ snapcase-example/cases/floating-golden/target.lua
@@ -1,13 +1,13 @@
 vim.cmd("enew")
 vim.fn.setline(1, {
   "Main Buffer",
-  "",
+  "(New message)",
   "Press ? for help",
   "Press q to quit",
 })
 
 local buf = vim.api.nvim_create_buf(false, true)
-local lines = { "Palette", "--------", "alpha", "beta", "gamma" }
+local lines = { "Palette", "--------", "one", "two", "three" }
 vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 
 vim.api.nvim_open_win(buf, false, {
