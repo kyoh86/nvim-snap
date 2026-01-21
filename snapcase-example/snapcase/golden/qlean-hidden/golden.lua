b863604b02bfaf18054c73dd7b1502221be0c7fb
kyoh86 <me@kyoh86.dev>
2026-01-21 11:46:51 +0900

    fix: align golden qlean scenarios

--- snapcase-example/snapcase/golden/qlean-hidden/golden.lua
+++ snapcase-example/snapcase/golden/qlean-hidden/golden.lua
@@ -1,3 +1,18 @@
+vim.pack.add({ "https://github.com/kyoh86/qlean.nvim" }, { confirm = false })
+
+local rule = require("qlean.rule")
+require("qlean").setup({
+  keep = rule.any(rule.buftype("", "acwrite", "terminal"), rule.filetype("fern")),
+})
+
+vim.go.hidden = true
+
+vim.cmd.new()
 vim.fn.setline(1, { "foobar" })
+vim.cmd.wincmd("c")
+
+vim.cmd.copen()
+vim.cmd.wincmd("k")
+pcall(vim.api.nvim_cmd, { cmd = "quit" }, { output = true })
 vim.cmd.redraw()
 require("nvim_snap").done()
