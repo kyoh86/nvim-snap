4001df1c37768678be823bcbdef7f1dad3e46dd1
kyoh86 <me@kyoh86.dev>
2026-01-25 18:41:54 +0900

    test: add diff golden example case

---
+++ snapcase-example/snapcase/golden/diff-golden/golden.lua
@@ -0,0 +1,11 @@
+vim.cmd("enew")
+vim.fn.setline(1, {
+  "diff-golden: baseline",
+  "alpha",
+  "beta",
+  "gamma",
+  "removed",
+  "delta",
+  "epsilon",
+  "zeta",
+})
---
+++ snapcase-example/snapcase/golden/diff-golden/target.lua
@@ -0,0 +1,11 @@
+vim.cmd("enew")
+vim.fn.setline(1, {
+  "diff-golden: target",
+  "alpha",
+  "beta changed",
+  "gamma",
+  "delta",
+  "epsilon",
+  "zeta",
+  "added",
+})
